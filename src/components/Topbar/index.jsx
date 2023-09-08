bc4f6720a81b42eccf08d568f64db91c4087a152
Rafael-Arruda <rafaelsilvaarruda72@gmail.com>
2023-09-08 18:19:30 -0300

    feat: elemento de perfil da topbar

--- src/components/Topbar/index.jsx
+++ src/components/Topbar/index.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Header, Search } from './style';
+import { 
+    Header, 
+    Search, 
+    Profile, 
+    Menu,
+    DropdownProfile 
+} from './style';
 
-import { MdSearch } from 'react-icons/md';
+import imgProfile from '../../assets/profile_img.jpg';
+
+import { MdSearch, MdOutlinePerson } from 'react-icons/md';
 
 export default function Topbar() {
+
+    const [showMenuProfile, setShowMenuProfile] = useState(false);
+
+    function handleShowMenuProfile() {
+        setShowMenuProfile(!showMenuProfile);
+    }
+
     return(
         <Header>
             <Search>
@@ -13,6 +28,37 @@ export default function Topbar() {
                     <MdSearch size={22} color="#b4bdce"/>
                 </button>
             </Search>
+            
+           <Menu>
+                <Profile onClick={handleShowMenuProfile}>
+                    <img src={imgProfile} alt="imagem de perfil" />
+                </Profile>
+
+                {showMenuProfile && 
+                <DropdownProfile>                    
+                    <div className="box-profile-info">
+                        <h5>Sandra Scotland</h5>
+                        <span>Administrator</span>
+                    </div>
+                    <a href="#">
+                        <MdOutlinePerson size={20} color="#000"/>
+                        <span>Perfil</span>
+                    </a>
+                    <a href="#">
+                        <MdOutlinePerson size={20} color="#000"/>
+                        <span>Ajuda</span>
+                    </a>
+                    <a href="#">
+                        <MdOutlinePerson size={20} color="#000"/>
+                        <span>Configurações</span>
+                    </a>
+                    <a href="#">
+                        <MdOutlinePerson size={20} color="#000"/>
+                        <span>Sair</span>
+                    </a>    
+                </DropdownProfile>
+                }
+           </Menu>
         </Header>
     )
 }
--- src/components/Topbar/style.js
+++ src/components/Topbar/style.js
@@ -4,9 +4,11 @@ export const Header = styled.header`
     background-color: var(--white);
     display: flex;
     align-items: center;
+    justify-content: space-between;
    
-    width: 100%;
+    width: calc(100vw - 260px);
     height: 12vh;
+    padding: 0px 40px;
     border-left: 1px solid var(--border-color);
     border-bottom: 1px solid var(--border-color);
     
@@ -17,7 +19,6 @@ export const Header = styled.header`
 `;
 
 export const Search = styled.div`
-    margin-left: 50px;
     border: 1px solid var(--border-color);
     display: flex;
     align-items: center;
@@ -41,5 +42,59 @@ export const Search = styled.div`
         padding: 5px;
         background: none;
         border: none;
+    }
+`;
+
+export const Profile = styled.button`
+    border: none;
+    background: none;
+
+    img {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+    }
+`;
+
+export const Menu = styled.div`
+    position: relative;
+`;
+
+export const DropdownProfile = styled.div`
+    position: absolute;
+    top: 55px;
+    right: 0;
+    display: flex;
+    flex-direction: column;
+
+    width: 200px;
+    background: var(--white);
+    border: 2px solid var(--border-color);
+    border-radius: 5px;
+
+    .box-profile-info {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding: 1rem;
+    }
+
+    .box-profile-info span {
+        color: #76839a;
+        font-size: 12px;
+        font-weight: 500;
+    }
+
+    a {
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        color: #000;
+        padding: 16px;
+        border-top: 1px solid var(--border-color);
+    }
+    a:hover {
+        background: rgba(98, 89, 202, 0.1);
     }
 `;
