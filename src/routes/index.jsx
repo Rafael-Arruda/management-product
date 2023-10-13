dc0725a4a21d6aadce9f5ebd0a1edb91ebc59f34
Fernando <fpinheiro@nwsoft.com.br>
2023-10-13 16:52:22 -0300

    feat: layout tela movimentação

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,23 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
 
 //Pages
-import Empty from "../pages/Empty";
-import Home from "../pages/Home";
-import Login from '../pages/Login';
-import Register from '../pages/Register';
-import TipoServico from '../pages/CadastroBase/Servico/TipoServico';
+import Baixa from '../pages/Almoxarifado/Material';
 import Estoque from '../pages/CadastroBase/Almoxarifado/Estoque';
 import Material from '../pages/CadastroBase/Almoxarifado/Material';
 import Unidade from '../pages/CadastroBase/Almoxarifado/Unidade';
-import Cargo from '../pages/CadastroBase/Perfil/Cargo';
-import MetodoPagamento from '../pages/CadastroBase/Financeiro/MetodoPagamento';
 import InstituicaoPagamento from '../pages/CadastroBase/Financeiro/InstituicaoPagamento';
+import MetodoPagamento from '../pages/CadastroBase/Financeiro/MetodoPagamento';
+import Cargo from '../pages/CadastroBase/Perfil/Cargo';
 import CentroCusto from '../pages/CadastroBase/Perfil/CentroCusto';
-import ListIcons from '../pages/Debug/ListIcons';
-import Page404 from '../pages/ErrorPage/404';
-import Layout from '../pages/Layout';
-import Service from '../pages/Servico';
 import Funcionario from '../pages/CadastroBase/Perfil/Funcionario';
+import TipoServico from '../pages/CadastroBase/Servico/TipoServico';
+import ListIcons from '../pages/Debug/ListIcons';
+import Empty from "../pages/Empty";
+import Page404 from '../pages/ErrorPage/404';
+import Home from "../pages/Home";
+import Layout from '../pages/Layout';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Service from '../pages/Servico';
 
 function RoutesApp() {
     return (
@@ -28,7 +29,8 @@ function RoutesApp() {
                 {/* <Route path="/cadastroMaterial" element={<RegisterMaterial />} /> */}
                 <Route path="/service" element={<Service />} />
                 <Route path="/servico/novo" element={<Service reg={{}} />} />
-                <Route path="/listicons" element={<ListIcons />} />
+                <Route path="/almoxarifado/baixa/entrada" element={<Baixa tipoMovimentacao='entrada' />} />
+                <Route path="/almoxarifado/baixa/saida" element={<Baixa tipoMovimentacao='saida' />} />
                 <Route path="/cadastro-base/servico/tipo-servico" element={<TipoServico />} />
                 <Route path="/cadastro-base/almoxarifado/unidade" element={<Unidade />} />
                 <Route path="/cadastro-base/almoxarifado/material" element={<Material />} />
@@ -38,6 +40,9 @@ function RoutesApp() {
                 <Route path="/cadastro-base/perfil/centro-custo" element={<CentroCusto />} />
                 <Route path="/cadastro-base/perfil/cargo" element={<Cargo />} />
                 <Route path="/cadastro-base/perfil/funcionario" element={<Funcionario />} />
+                
+                
+                <Route path="/listicons" element={<ListIcons />} />
             </Route>
 
             <Route path="/login" element={<Login />} />
